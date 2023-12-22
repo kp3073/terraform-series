ac1f10742eb48b5589c119dd9661777f1045d39b
keyurpatel <keyurpatel8288@gmail.com>
2023-12-22 15:49:44 -0500

    hi

---
+++ main.tf
@@ -0,0 +1,28 @@
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "AKIAYYSPFTVLGAGWFDM3"
+  secret_key = "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"
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = var.instance_type
+  count = var.count2
+
+  tags = {
+    Name = "terraform"
+  }
+}
+
+variable "instance_type"{
+  description = "instance type"
+  type = string
+  default = "t3.medium"
+}
+
+variable "count2" {
+  description = "number of instance"
+  type = number
+  default = 2
+}
+~
