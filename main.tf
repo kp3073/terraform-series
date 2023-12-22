c6392d2124fcfa40ea0ed3c9882989e388d45fa9
keyurpatel <keyurpatel8288@gmail.com>
2023-12-22 15:58:36 -0500

    hih

--- main.tf
+++ main.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "web" {
   ami = "ami-03265a0778a880afb"
   instance_type = var.instance_type
   count = var.count2
+  associate_public_ip_address = var.enable_ip
 
   tags = {
     Name = "terraform"
@@ -19,4 +20,10 @@ variable "count2" {
   description = "number of instance"
   type = number
   default = 2
+}
+
+variable "enable_ip" {
+  description = "Enable ip address"
+  type = bool
+  default = true
 }
